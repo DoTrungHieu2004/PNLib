ac1746970c9df4996fbda9258116c0616096f600
DoTrungHieu2004 <hieu10productions@gmail.com>
2025-08-25 20:14:56 +0700

    Thiết kế các màn hình thống kê

---
+++ app/src/main/java/com/hieu10/pnlib/screens/statistics/DoanhThuActivity.java
@@ -0,0 +1,16 @@
+package com.hieu10.pnlib.screens.statistics;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hieu10.pnlib.R;
+
+public class DoanhThuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doanh_thu);
+    }
+}
